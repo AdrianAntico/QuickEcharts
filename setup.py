ca6c3e62595b2a7a1568ef521dd0918661991caa
Adrian Antico <adrianantico@gmail.com>
2024-03-05 14:46:08 -0700

    Package up repo

--- setup.py
+++ setup.py
@@ -14,16 +14,15 @@ with open(os.path.join(HERE, "requirements.txt")) as f:
     required = f.read().splitlines()
 
 setup(
-    name="AutoEcharts",
+    name="QuickEcharts",
     version="0.1.0",
     description="Create Echart plots in a single simple function call, with internal data wrangling via polars",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/AdrianAntico/AutoEcharts",
+    url="https://github.com/AdrianAntico/QuickEcharts",
     author=["Adrian Antico"],
     author_email="adrianantico@gmail.com",
-    license="AGPL (>= 3)",
-    packages=find_packages(),
+    license="AGPL >= 3",
     install_requires=required,
     classifiers=[
         "Programming Language :: Python",
