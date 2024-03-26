4344a95d9b555fe4e2e5a334af8174cf3024ee2a
Adrian Antico <adrianantico@gmail.com>
2024-03-26 13:14:25 -0700

    Updates

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(os.path.join(HERE, "requirements.txt")) as f:
 
 setup(
     name="QuickEcharts",
-    version="0.1.0",
+    version="1.0.0",
     description="Create Echart plots in a single simple function call, with internal data wrangling via polars",
     long_description=README,
     long_description_content_type="text/markdown",
